c661469c942bd60123fd5c420f5dbfad4076276a
Yue Xie <moonxie9304@gmail.com>
2025-02-11 21:55:19 +0800

    updated functions

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -5,11 +5,12 @@ export default function Authenitcate({token}) {
     const [error, setError] = useState(null)
     async function handleClick() {
         try {
+            console.log(token)
             const response = await fetch("https://fsa-jwt-practice.herokuapp.com/authenticate", {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
-                    Authorization: `Bearer${token}`
+                    Authorization: `Bearer ${token}`
                 },
             })
             const result = await response.json()
