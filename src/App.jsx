0d0ca7a449380e67b4e4b609a79f7c11026ccef6
Yue Xie <moonxie9304@gmail.com>
2025-02-13 03:27:08 +0800

    fixedAuthenticate

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
-import Authenitcate from './components/Authenticate'
+import Authenticate from './components/Authenticate'
 import SignUpForm from './components/SignUpForm'
 import React, { useState } from 'react'
 
 function App() {
   const [token, setToken] = useState(null)
+  const [activeUser, setActiveUser] = useState('')
   return (
     <>
-        <Authenitcate token={token} setToken={setToken}/>        
-        <SignUpForm token={token} setToken={setToken}/>
+        <h1>React Form</h1>
+        <SignUpForm setToken={setToken} setActiveUser={setActiveUser} />
+        <Authenticate token={token} activeUser={activeUser} />        
     </>
   )
 }
